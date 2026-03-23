0bdba05650ae92ccc5ac29e0f6b395067f8608a3
Rekha Takkalapally <takkalapallyr@gmail.com>
2026-03-23 19:29:53 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("SMS Sent Successfully!");
+        System.out.println("SMS Sent Successfully 9121220327");
     }
 }
